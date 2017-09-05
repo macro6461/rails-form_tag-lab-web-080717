aa01f9705869948adac178d88e05a063c05916fe
macro6461 <mattcroak718@gmail.com>
2017-09-05 07:31:22 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,22 @@
+require 'pry'
 class StudentsController < ApplicationController
-  
+
   def index
-    @students = Student.all
+    @students = Student.all #@students still nil??
+  end
+
+  def new
+    #upon redirect_to create, first and last name were nil.
+  end
+
+  def create
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to students_path #/students
   end
 
   def show
     @student = Student.find(params[:id])
   end
+
 
 end
